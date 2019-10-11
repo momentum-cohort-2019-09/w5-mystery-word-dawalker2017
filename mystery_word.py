c54c813b8849b857e0ed3ffd395175a5e2533c2d
danielwalker <dawalker2017@outlook.com>
2019-10-11 19:30:30 -0400

    10.11.19 07:28PM Main Game working

--- mystery_word.py
+++ mystery_word.py
@@ -1,6 +1,7 @@
 # Predetermined Python module, 'import' calls the module for use throughout the code.
 import random
-#==================================================================================================| Reading the File |==============================#
+
+#==================================================================================================| READ FILE |=====================================#
 # Creating a function that reads the file.
 
 
@@ -9,7 +10,7 @@ def read_the_file(filename):
         text = file.read()
         return make_list_from_text(text)
 
-#==================================================================================================| Making a List of the Text |=====================#
+#==================================================================================================| MAKE TEXT INTO LIST |===========================#
 # Creating a function that puts words into a list and trims any space around each word.
 
 
@@ -17,37 +18,124 @@ def make_list_from_text(every_word):
     all_words = every_word.split()
     return all_words
 
-#==================================================================================================| Get a Random Word |=============================#
-# Creating a function that picks a random word out of the list of words.
+#==================================================================================================| NEW GAME |======================================#
+# Creating a function that displays the 'starting new game' introduction.
+
+
+def new_game():
+    starting_input = input(
+        "\n               ?¿? MYSTERY WORD ?¿? \n Guess a letter to complete the word, Press 'ENTER' to start! \n")
+    if starting_input == "":
+        difficulty_level = input(
+            "SELECT DIFFICULTY: e = Easy, m = Medium, h = Hard").lower()
+        if difficulty_level == "e":
+            print("EASY MODE")
+        if difficulty_level == "m":
+            print("MEDIUM MODE")
+        if difficulty_level == "h":
+            print("HARD MODE")
+    else:
+        print("Need Valid Input")
+    return difficulty_level
+
+#==================================================================================================| GET RANDOM WORD/REPLACE LETTERS |===============#
+# Creating a function that picks a random word out of the list of words and replaces its letters with " _ ".
 
 
 def get_word(list_of_words):
-    random_word = random.choice(list_of_words)
-    print(random_word)
-    return random_word
+    random_word = random.choice(list_of_words).lower()
+    current_word = (("[ _ ] ") * len(random_word))
+    print("=== " + current_word + "===")
+    print(random_word + '\n')
+    return(random_word)
 
-#==================================================================================================| Guess a Letter |================================#
+#==================================================================================================| DIFFICULTY |====================================#
+
+
+def word_level(words, difficulty):
+    easy = []
+    moderate = []
+    hard = []
+
+    for word in words:
+        if len(word) >= 4 and len(word) <= 6:
+            easy.append(word)
+        elif len(word) > 6 and len(word) <= 8:
+            moderate.append(word)
+        elif len(word) > 8:
+            hard.append(word)
+    if difficulty == "e":
+        index = random.randrange(len(easy) + 1)
+        return easy
+    elif difficulty == "m":
+        index = random.randrange(len(moderate) + 1)
+        return moderate
+    elif difficulty == "h":
+        index = random.randrange(len(hard) + 1)
+        return hard
+
+#==================================================================================================| SHOW LETTERS |==================================#
+# Creating a function that, if a guess of a letter is correct, then add it to the word. Otherwise, keep it a "_".
+
+
+def display_letter(word, correct_guesses):
+    display_word = " "
+    for letter in word:
+        if letter in correct_guesses:
+            display_word += letter + " "
+        else:
+            display_word += "[ _ ]"
+    print(display_word)
+    return display_word
+
+#==================================================================================================| GUESS LETTER/CORRECT OR INCORRECT |=============#
 # Creating a function to allow the User to guess a letter and determine wether it is correct/incorrect.
 
 
-def guess_a_letter(word):
-    guessed_letter = input("Guess a Letter")
-    print(guessed_letter)
+def correct_or_incorrect_guess(word, correct_guesses, incorrect_guesses, lives):
+    letter = input("Guess a Letter: ")
+    if letter in word:
+        correct_guesses.append(letter)
+        print('\n' + "Correct!")
+    else:
+        incorrect_guesses.append(letter)
+        lives -= 1
+        print('\n' + "Try Again!")
+        print(f" {lives} LIVES REMAINING")
+    display_letter(word, correct_guesses)
+    return correct_guesses, incorrect_guesses
 
-#==================================================================================================#
-# Define the function of "is currently playing this game".
-    # Define current state of "is playing this game".
-    # Define empty array(list) for correct guesses.
-    # Define empty array(list) for incorrect guesses.
-
-    # Create a while loop of previously defined state AND length of User's incorrect guesses.
-    # Pass through the while loop: 'state_of_game' AND 'length of incorrect guesses' [less than] 'chosen number'...
-    # Call the 'guess_letter' function as previously created.('word', 'incorrect_guesses', 'correct_guesses').
+#==================================================================================================| WINNING GAME |==================================#
+# Creating a function that checks if the User has won the game yet.
 
 
-#==================================================================================================#
+def has_player_won(word, correct_guesses):
+    for letter in word:
+        if letter not in correct_guesses:
+            return False
+    return True
+
+#==================================================================================================| GAME DURATION |=================================#
+# Creating a function that uses a while loop that WHILE the 'game is playing' tracks number of incorrect guesses
+# the users trys and after 10 is hit, the game ends
+
+
+def during_game():
+    currently_playing = True
+    correct_guesses = []
+    incorrect_guesses = []
+    difficulty = new_game()
+    word = get_word(word_level(read_the_file('words.txt'), difficulty))
+    lives = 6
+
+    while currently_playing and len(incorrect_guesses) < 6:
+        correct_or_incorrect_guess(word, correct_guesses, incorrect_guesses, lives)
+        if has_player_won(word, correct_guesses):
+            currently_playing = False
+            print("YOU WIN!")
+    print("Game Over!")
+
+
+#====================================================================================================================================================#
 # "words" is delacred as 'read_the_file' function as created above.
-words = read_the_file('words.txt')
-
-# Calling the 'guess_a_letter' function on the 'get_word' function previously created.
-guess_a_letter(get_word(words))
+during_game()
